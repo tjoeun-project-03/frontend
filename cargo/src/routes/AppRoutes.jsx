6c2a8e52c1c7289d64cde0d734a5162fdc7a1f0b
KHW <elelte991@tukorea.ac.kr>
2026-02-10 18:29:03 +0900

    경로 추가

--- cargo/src/routes/AppRoutes.jsx
+++ cargo/src/routes/AppRoutes.jsx
@@ -3,24 +3,27 @@ import { Routes, Route } from 'react-router-dom';
 
 import { LoginPage } from '../pages/LoginPage';
 import {DashboardPage} from '../pages/DashboardPage';
-import { InquiryList } from '../pages/support/InquiryList';
-import { InquiryDetail } from '../pages/support/InquiryDetail';
-import { NoticeWrite } from '../pages/support/NoticeWrite';
-import { NoticeList } from '../pages/support/NoticeList';
-import { NoticeDetail } from '../pages/support/NoticeDetail';
+import { InquiryList } from '../pages/inquiry/InquiryList';
+import { InquiryDetail } from '../pages/inquiry/InquiryDetail';
+import { NoticeWrite } from '../pages/notice/NoticeWrite';
+import { NoticeList } from '../pages/notice/NoticeList';
+import { NoticeDetail } from '../pages/notice/NoticeDetail';
+import { ReportList } from '../pages/report/ReportList';
+import { ReportDetail } from '../pages/report/ReportDetail';
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} /> {/* 로그인 페이지 */}
       <Route path="/dashboard" element={<DashboardPage />} /> {/* 대시보드 (관리자 메인) */}
-      <Route path="/support/inquiry" element={<InquiryList/>}/> {/* 1:1 문의 리스트 */}
-      <Route path="/support/inquiry/:id" element={<InquiryDetail/>}/> {/* 1:1 문의 디테일 */}
-      <Route path="/support/notice" element={<NoticeList/>}/>{/* 공지사항 리스트 */}
-      <Route path="/support/notice/write" element={<NoticeWrite/>}/>{/* 공지사항 작성 */}
-      <Route path="/support/notice/:id" element={<NoticeDetail/>}/>{/* 공지사항 상세보기 */}
-      {/* (선택사항) 공지사항 수정 - 지금은 등록 페이지를 같이 써도 됩니다 */}
-      <Route path="/support/notice/write/:id" element={<NoticeWrite />} />
+      <Route path="/inquiry" element={<InquiryList/>}/> {/* 1:1 문의 리스트 */}
+      <Route path="/inquiry/:id" element={<InquiryDetail/>}/> {/* 1:1 문의 디테일 */}
+      <Route path="/notice" element={<NoticeList/>}/>{/* 공지사항 리스트 */}
+      <Route path="/notice/write" element={<NoticeWrite/>}/>{/* 공지사항 작성 */}
+      <Route path="/notice/:id" element={<NoticeDetail/>}/>{/* 공지사항 상세보기 */}
+      <Route path="/notice/write/:id" element={<NoticeWrite />} /> {/* 공지사항 수정 */}
+      <Route path="/report" element={<ReportList/>}/>{/* 신고목록 리스트 */}
+      <Route path="/report/:id" element={<ReportDetail/>}/>{/* 신고 상세보기 */}
     </Routes>
   );
 };
