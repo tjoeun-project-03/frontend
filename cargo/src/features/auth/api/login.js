634c7b6c48e18a38e525d71aa26b5afa76f2447c
KHW <elelte991@tukorea.ac.kr>
2026-02-10 18:27:49 +0900

    login 부분 합치기

--- cargo/src/features/auth/api/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const loginUser = async (id, password) => {
-  const response = await axios.post('http://localhost:8080/api/auth/login', {
-    userId: id,
-    userPw: password,
-  });
-  return response.data;
-};
